98ecb7a963d2ebe5e2d25881ebcc988fe8d1f0b9
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-01-25 04:11:05 +0530

    ignore deploy error

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const PORT=process.env.PORT||8081
 
 
 app.use(cors({
-    origin: 'https://unrivaled-malasada-ac96ba.netlify.app/',
+    origin: 'https://ask-project-aman.netlify.app/Question',
     credentials: true,
     methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
-    allowedHeaders: ['Content-Type','*'],
+    allowedHeaders: ['Content-Type'],
     exposedHeaders: ['Content-Type']
 }))
 app.use(express.static("/profile_pic"))
