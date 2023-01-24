fb26bcd59ebd630dcf9b5769f923e8b3bccf4df8
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-01-25 00:16:13 +0530

    cors

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ require("./db/conn")
 app.use(cookieParser())
 
 app.use(require("./router/routingask"))// middleware where all the routes are defined or all the Api's are defined
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log("connected to port number 8081"))//creting a port number 8081
+app.listen(PORT, () => console.log(`connected to port number ${PORT}`))//creting a port number 8081
