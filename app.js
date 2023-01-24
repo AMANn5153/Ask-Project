1d0de4e9588dce712699e8b056eb7347dbd40088
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-01-25 00:29:08 +0530

    cors

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const dotenv=require("dotenv")
 const cookieParser=require("cookie-parser")
+const cors=require("cors")
 const PORT=process.env.PORT||8081
 
 
-
+app.use(cors({
+    origin:"https://cerulean-bavarois-f1b421.netlify.app/"
+}))
 app.use(express.static("/profile_pic"))
 
 dotenv.config({path:"./config.env"})
@@ -15,13 +18,7 @@ require("./db/conn")
 app.use(cookieParser())
 
 app.use(require("./router/routingask"))// middleware where all the routes are defined or all the Api's are defined
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log(`connected to port number ${PORT}`))//creting a port number 8081
