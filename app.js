1537fcdbe818a5471c8d6a88ff2d18d840a0af15
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-01-25 13:44:51 +0530

    ignore deploy error cors

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const PORT=process.env.PORT||8081
 
 
 app.use(cors({
-    origin: 'https://ask-project-aman.netlify.app/Question',
+    origin:function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins)
+    })
+  },
     credentials: true,
     methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
     allowedHeaders: ['Content-Type'],
