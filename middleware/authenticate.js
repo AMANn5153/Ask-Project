13e0d9233283f80d409a5baf45a3c59029faf725
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-02-02 03:39:09 +0530

    last

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,6 +5,7 @@ const {Testbackend}=require("../models/models")
 const authenticate= async (req,res,next)=>{
     try{
     const token= req.cookies.auth// taking token
+    console.log(req)
     
     const authnToken= jwt.verify(token,process.env.SECRET)//verfify token with secret key{token is made up of user unique id and secret key} return unique id
     const userInfo= await Testbackend.find({_id:authnToken._id},{"tokens.token":token})//finding document that matches the unique id and token 
