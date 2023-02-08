4ca413b537c67f94fb3093586beaa5cd2f2f28a1
AMANn5153 <93911358+AMANn5153@users.noreply.github.com>
2023-02-08 19:42:12 +0530

    deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static("/client/build"))
 
     app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
